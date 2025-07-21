15146624bc7c1c06ef8af3d055401841751c187a
Sweta <Sweta-singh-123@gmail.com>
2025-07-21 09:25:03 +0545

    last commit

--- init/index.js
+++ init/index.js
@@ -1,25 +1,50 @@
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// const MONGO_URL = "mongodb+srv://sweta:SafeP%40ss2025%21%24@cluster0.if7zvxw.mongodb.net/wanderlust?retryWrites=true&w=majority";
+
+// main()
+//   .then(() => {
+//     console.log("connected to DB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// }
+
+// const initDB = async () => {
+//   await Listing.deleteMany({});
+//   await Listing.insertMany(initData.data);
+//   console.log("data was initialized");
+// };
+
+// initDB();
+
+
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb+srv://sweta:SafeP%40ss2025%21%24@cluster0.if7zvxw.mongodb.net/wanderlust?retryWrites=true&w=majority";
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("Connected to DB");
+
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+
+    mongoose.connection.close();  // close connection after seeding
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-const initDB = async () => {
-  await Listing.deleteMany({});
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
-};
-
-initDB();
+main();
